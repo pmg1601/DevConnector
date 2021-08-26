21830d51a452af1059aeeeabac1be6e41e7b760f
Pratik <pratikghodke4@gmail.com>
2021-08-26 19:05:39 +0530

    Added 404 Not Found

---
+++ frontend/src/components/layout/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className='x-large text-primary my-3'>
+                <i className='fas fa-exclamation-triangle'></i> Page Not Found
+                <p className='large my-3'>
+                    Sorry, this page does not exists! :(
+                </p>
+            </h1>
+        </div>
+    )
+}
+
+export default NotFound
